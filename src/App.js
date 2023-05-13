f5faff9187167f367b2fb68c2f36c825e27f3eda
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-14 00:12:41 +0530

    navbar added to page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
+import './App.css'
 
 function App() {
-  return ( 
-    <div className="App">
-
+  return (
+    <div className="App shadow-sm ">
+      <Navbar />
     </div>
   );
 }
