60374b2c41e2d8485123ccf8d900e3bc82eee19c
harshbhange0 <harshbhange123@gmail.com>
2023-05-13 23:19:26 +0530

    data js file added

--- src/Components/question/CQuestion.js
+++ src/Components/question/CQuestion.js
@@ -1,21 +1,24 @@
 //don't forget to add comma after the objects property's value
+// ans property will be same as options value
+
+// if question 1 ans is int variableName then the ans value well be 1
+// as shown in following
 
 // array start
 const cLanguage = [
-    //object star add question as shown
-    {
-        //id is important
-        question: "What is the syntax for declaring a variable in C?",
-        ans: 1,
-        //option will be an array
-        option: [
-            "int variableName",
-            "veriableName = int",
-            "variableName int",
-            "none of the above",
-        ],
-    },
-    //object end add question as shown
-
+  //object star add question as shown
+  {
+    question: "What is the syntax for declaring a variable in C?",
+    //ans value is 1 and the option 1 value is answer
+    ans: 1,
+    //option will be an array
+    option: [
+      "int variableName",//this is ans
+      "veriableName = int",
+      "variableName int",
+      "none of the above",
+    ],
+  },
+  //object end add question as shown
 ];
 //array end
