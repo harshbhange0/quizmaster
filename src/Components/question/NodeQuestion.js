706519630eebdab9ec6c067aea1b4e5bb024dfc3
Tejas Santosh Sonawane <sonawanetejas712@gmail.com>
2023-05-14 13:22:38 +0530

    add node.js component

---
+++ src/Components/question/NodeQuestion.js
@@ -0,0 +1,103 @@
+const nodelanguage = [
+    {
+        id: 1,
+        question:"Which of the following statement are true?",
+        ans: 1,
+        option: [
+            "Node.js is a server-side language.",
+            "Node.js is client-side laguage.",
+            "Node.js is both a sever-side & client-side language.",
+            "None of the above.",
+        ]
+    },
+    {
+        id: 2,
+        question:"Node.js is written in which language?",
+        ans: 2,
+        option: [
+            "c++",
+            "javascript",
+            "c",
+            "java",
+        ]
+    },
+    {
+        id: 3,
+        question:"How many node objects methods are available?",
+        ans: 2,
+        option: [
+            "21",
+            "18",
+            "19",
+            "20",
+        ]
+    },
+    {
+        id: 4,
+        question:"How can we check the installed version of Node?",
+        ans: 2,
+        option: [
+            "npm-version",
+            "node-version",
+            "npm getversion",
+            "None of the above",
+        ]
+    },
+    {
+        id: 5,
+        question:"Which of the following is not a valid language for Node.js?",
+        ans: 20,
+        option: [
+            "javascript",
+            "java",
+            "c",
+            "c++",
+        ]
+    },
+    {
+        id: 6,
+        question:"Which of the following shortcut command is used to kill a process in Node.js?",
+        ans: 4,
+        option: [
+            "Ctrl + B",
+            "Ctrl + K",
+            "Ctrl + T",
+            "Ctrl + C",
+        ]
+    },
+    {
+        id: 8,
+        question:"What is the default scope in Node.js application?",
+        ans: 2,
+        option: [
+            "Global",
+            "Local",
+            "Global Function",
+            "Local to object",
+        ]
+    },
+    {
+        id: 9,
+        question:"Node.js is ________ by default ?",
+        ans: 3,
+        option: [
+            "Synchronous",
+            "Multi-threaded",
+            "Asynchronous",
+            "None of the above",
+        ]
+    },
+    {
+        id: 10,
+        question:"How to test if two nodes are equal?",
+        ans:1,
+        option: [
+            "isEqualNode()",
+            "equal()",
+            "==",
+            "None of the above",
+
+
+        ]
+    },
+]
