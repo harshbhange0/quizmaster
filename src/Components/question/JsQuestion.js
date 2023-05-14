229156f2a647eb999437e952c985c22a6cbca3b2
RuchikaKadu02 <kaduruchika@gmail.com>
2023-05-14 00:03:32 -0700

    js question modified

--- src/Components/question/JsQuestion.js
+++ src/Components/question/JsQuestion.js
@@ -1,13 +1,13 @@
 const jsLanguage = [
   {
     question: "Inside which element do we put the Javascript?",
-    ans: 1,
+    ans: 0,
     option: ["<script>", "<scripting>", " <js>", "<javascript>"],
   },
 
   {
     question: "Javascript is an ------- language?",
-    ans: 1,
+    ans: 0,
     option: [
       "Object Oriented",
       "Object Based",
@@ -19,14 +19,14 @@ const jsLanguage = [
   {
     question:
       "Which of the following keywords is used to define a variable in Javascript?",
-    ans: 3,
+    ans: 2,
     option: ["Var", " Let", " Both A and B", " None of Above"],
   },
 
   {
     question:
       "Which of the following methods can be used to display data in some form using Javascript?",
-    ans: 4,
+    ans: 3,
     option: [
       "document.write()",
       "console.log()",
@@ -37,7 +37,7 @@ const jsLanguage = [
 
   {
     question: "What is the use of the <noscript> tag in Javascript?",
-    ans: 1,
+    ans: 0,
     option: [
       "The content are displayed by Non-Js-based browsers",
       " Clear all the cookies and Caches",
@@ -49,14 +49,14 @@ const jsLanguage = [
   {
     question:
       "When an operator's value is NULL, the typeof returned by the unary operator is:",
-    ans: 3,
+    ans: 2,
     option: ["Boolean", " Undefined", " Object", "Integer"],
   },
 
   {
     question:
       "The process in which an object or data structure is translated into a format suitable for transferral over a network, or storage is called?",
-    ans: 1,
+    ans: 0,
     option: [
       "Object serialisation",
       " Object Encapsulation",
@@ -67,7 +67,7 @@ const jsLanguage = [
 
   {
     question: "What does the 'toLocateString()' method do in JS?",
-    ans: 3,
+    ans: 2,
     option: [
       "Returns a localised object representation.",
       " Returns a parsed string.",
@@ -79,13 +79,13 @@ const jsLanguage = [
   {
     question:
       "Which function is used to serialize an object into a JSON string in Javascript?",
-    ans: 1,
+    ans: 0,
     option: ["stringify()", " Parse()", " Convert()", " None of Above"],
   },
 
   {
     question: "Which of the following is not a Javascript framework?",
-    ans: 3,
+    ans: 2,
     option: ["Node", " Vue", "React", "Cassandra"],
   },
 ];
