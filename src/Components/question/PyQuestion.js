9dfc0fc53d324cbb0984232f0d8d546c13c1be0c
nikitajambe <nikitajambe@gmail.com>
2023-05-14 11:38:41 +0530

    python question added

---
+++ src/Components/question/PyQuestion.js
@@ -0,0 +1,140 @@
+const PyLanguage =[
+    {
+        question1: "Who developed Python Programming Language?",
+
+        ans: "C",
+
+        option:[
+            "A)Wick van Rossum",
+            "B)Rasmus Lerdorf",
+            "c) Guido van Rossum",
+            "d) Niene Stom"
+
+        ],
+    },
+
+
+{
+    question2: "Which of the following is the correct extension of the Python file?",
+
+    ans: "c",
+
+    option:[
+        "a) .python",
+        "b) .pl",
+        "c) .py",
+        "d) .p",
+    ],
+},
+
+
+{
+    question3: "Which type of Programming does Python support?",
+
+    ans: "D",
+
+    option:[
+        "a)  object-oriented programming",
+        "b) structured programming",
+        "c) functional programming",
+        "d) all of the mentioned",
+    ],
+},
+
+
+{
+    question4: "Which of the following statements is used to create an empty set in Python?",
+
+    ans: "D",
+
+    option:[
+        "a)  ()",
+        "b) []",
+        "c) {}",
+        "d) set()",
+    ],
+},
+
+
+{
+    question5: "Which of the following character is used to give single-line comments in Python?",
+
+    ans: "B",
+
+    option:[
+        "a)  //",
+        "b) #",
+        "c) !",
+        "d) /*",
+    ],
+},
+
+{
+    question6: "Which of the following is used to define a block of code in Python language?",
+
+    ans: "A",
+
+    option:[
+        "a) Identation",
+        "b) Key",
+        "c) Brackets",
+        "d) All of the mentioned",
+    ],
+},
+
+
+{
+    question7: "Which of the following functions is a built-in function in python?",
+
+    ans: "B",
+
+    option:[
+        "a) factorial()",
+        "b) print()",
+        "c) speed()",
+        "d) sqrt()",
+    ],
+},
+
+
+{
+    question8: "What will be the output of the following Python function?",
+
+    ans: "B",
+
+    option:[
+        "a) pass",
+        "b)eval",
+        "c) assert",
+        "d) nonlocal",
+    ],
+},
+
+
+{
+    question9: "What does pip stand for python?",
+
+    ans: "C",
+
+    option:[
+        "a) Pip installs python",
+        "b) pip installs package",
+        "c) preferred installer program",
+        "d) all of the mention",
+    ],
+},
+
+
+{
+    question10: "Which of the following functions can help us to find the version of python that we are currently working on?",
+
+    ans: "D",
+   
+    option:[
+        "a) sys.version(1)",
+        "b) sys.version(0)",
+        "c) sys.version(2)",
+        "d) sys.version",
+    ],
+},
+];
