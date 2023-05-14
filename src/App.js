a55f5cb221002a3ca127800263eeca4ae3cdadbd
harshbhange0 <harshbhange123@gmail.com>
2023-05-14 23:45:26 +0530

    minimal bug fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 
-import './App.css'
-import QuestionSection from "./Components/QuestionSection";
-import SideBarSection from "./Components/SideBar";
-
-
 import "./App.css";
-
-import Offcanvas from "./Components/Offcanvas";
 import SideBarSection from "./Components/SideBar";
 import QuestionSection from "./Components/QuestionSection";
 
