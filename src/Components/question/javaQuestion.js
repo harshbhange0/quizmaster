0e4d669a29cb7c78329180a4e80489dce495ba74
SwetaRamteke-74 <swetaramteke38@gmail.com>
2023-05-14 00:30:44 -0700

    java que complete

---
+++ src/Components/question/javaQuestion.js
@@ -0,0 +1,124 @@
+const javaLanguage =[
+    {
+        question: "Number of primitive data types in Java are?stion ",
+        ans: 4, 
+        option: [
+             " 6",
+             " 7",
+             " 8",
+             " 8",
+         
+        ],
+      },
+
+      {
+        question: "To which of the following does the class string belong to. ",
+        ans: 1, 
+        option: [
+          "java.lang",
+          " java.awt",
+          " java.applet",
+          " java.string",
+         
+        ],
+      },
+
+      {
+        question: "Total constructor string class have? ",
+        ans: 3, 
+        option: [
+          "3",
+          "7",
+          "13",
+          "20",
+         
+        ],
+      },
+
+      {
+        question: "  Where does the system stores parameters and local variables whenever a method is invoked? ",
+        ans: 2, 
+        option: [
+          "Heap",
+          "Stack",
+          "Array ",
+          " Tree",
+         
+        ],
+      },
+
+      {
+        question: "What is the implicit return type of constructor?  ",
+        ans:2, 
+        option: [
+          "No return type",
+          " Aclass object in which it is defined",
+          " Void",
+          " None",
+         
+        ],
+      },
+
+      {
+        question: "Exception created by try block is caught in which block ",
+        ans: 1, 
+        option: [
+          "catch",
+          " throw",
+          "final",
+          " none",
+         
+        ],
+      },
+
+      {
+        question: "  Which of the following exception is thrown when divided by zero statement is executed?",
+        ans: 3, 
+        option: [
+          "NullPointrException",
+          " NumberFormatException",
+          " ArithmaticExceotion",
+          " None",
+         
+        ],
+      },
+
+      {
+        question: "  Where is System class defined? ",
+        ans: 1, 
+        option: [
+          "java.lang.package",
+          " java.util.package",
+          " java.io.package",
+          "None",
+         
+        ],
+      },
+
+      {
+        question: "  Identify the interface which is used to declare core methods in java ",
+        ans: 4, 
+        option: [
+          " Comparator",
+          " EventListener",
+          " Set",
+          " Collection",
+         
+        ],
+      },
+
+      {
+        question: "  Which of the following is used to find and fix bugs in the program? ",
+        ans: 4, 
+        option: [
+          "JDK",
+          " JRE",
+          " JVM",
+          " JDB",
+         
+        ],
+      },
+      
+    ];
+   
+    console.log(javaLanguage.question)
