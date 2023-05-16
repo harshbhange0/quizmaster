73989133d149c41e855a94207e88fc247930934e
sanika <sanikabargal7@gmail.com>
2023-05-16 18:34:27 +0530

    index number changed

--- src/Components/question/Bootstrap.js
+++ src/Components/question/Bootstrap.js
@@ -2,55 +2,55 @@ export const CQuestion = [
     {
       question:
         "Who developed the bootstrap?",
-      ans: 1,
+      ans:2,
       option: ["James Gosling", "Mark Otto and Jacob Thornton", "Mark Jukervich", "Dennis Ritchie"],
     },
     {
       question: "Which of the following class in Bootstrap is used to provide a responsive fixed width container?",
-      ans: 2,
+      ans:3,
       option: ["All of the above", ".container-fluid", ".container", ".container-fixed"],
     },
     {
       question: "The plugin used to create a cycle through elements as a slideshow is -",
-      ans: 2,
+      ans:3,
       option: ["carousel", "slideshow", "carousel", "None of these"],
     },
     {
       question: "The class in Bootstrap used to provide rounded corners to the image is",
-      ans: 3,
+      ans:4,
       option: [".image-rounded", ".image-circle", ".img-circle", ".img-rounded"],
     },
     {
       question: "Which of the following class in Bootstrap is used to indicate the uppercased text",
-      ans: 2,
+      ans:3,
       option: [".text-upper", ".text-capitalize", ".text-uppercase", ".uppercase"],
     },
     {
       question:
         "Is it true that the Bootstrap grid system works across multiple devices?",
-      ans: 0,
+      ans:1,
       option: ["True", "False", ],
     },
     {
       question: "What is the default layout of the Bootstrap form?",
-      ans: 2,
+      ans:4,
       option: ["a0Horizontal", " Vertical", "Inline", "None of the above"],
     },
     {
       question:
         "Which of the following class in Bootstrap is used to create a button as a link?",
-      ans: 2,
+      ans:3,
       option: ["None of the above", ".btn-hyperlink", ".btn-link", ".btn-anchor"],
     },
     {
       question: "The class xs in Bootstrap is used for ",
-      ans: 1,
+      ans:2,
       option: ["All of the above", "  phones  ", "tablets", "desktop"],
     },
     {
       question:
         "Which of the following statement is correct about the Bootstrap wells",
-      ans: 0,
+      ans:1,
       option:[
         "All of the above",
         "By default, the size of wells is medium.",
@@ -61,7 +61,7 @@ export const CQuestion = [
     {
         question:
         "How many columns are allowed in a bootstrap grid system?",
-        ans: 2,
+        ans:3,
         option:[
             "2", "12", "3", "5"
         ],
