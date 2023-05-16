68b7f1ad2c97ec15c925612d1dd4bfa9c30ad639
sanika <sanikabargal7@gmail.com>
2023-05-16 18:19:39 +0530

    Bootstrap ques added

---
+++ src/Components/question/Bootstrap.js
@@ -0,0 +1,61 @@
+export const CQuestion = [
+    {
+      question:
+        "Who developed the bootstrap?",
+      ans: 1,
+      option: ["James Gosling", "Mark Otto and Jacob Thornton", "Mark Jukervich", "Dennis Ritchie"],
+    },
+    {
+      question: "Which of the following class in Bootstrap is used to provide a responsive fixed width container?",
+      ans: 2,
+      option: ["All of the above", ".container-fluid", ".container", ".container-fixed"],
+    },
+    {
+      question: "The plugin used to create a cycle through elements as a slideshow is -",
+      ans: 2,
+      option: ["carousel", "slideshow", "carousel", "None of these"],
+    },
+    {
+      question: "The class in Bootstrap used to provide rounded corners to the image is",
+      ans: 3,
+      option: [".image-rounded", ".image-circle", ".img-circle", ".img-rounded"],
+    },
+    {
+      question: "Which of the following class in Bootstrap is used to indicate the uppercased text",
+      ans: 2,
+      option: [".text-upper", ".text-capitalize", ".text-uppercase", ".uppercase"],
+    },
+    {
+      question:
+        "Is it true that the Bootstrap grid system works across multiple devices?",
+      ans: 0,
+      option: ["True", "False", ],
+    },
+    {
+      question: "What is the default layout of the Bootstrap form?",
+      ans: 2,
+      option: ["a0Horizontal", " Vertical", "Inline", "None of the above"],
+    },
+    {
+      question:
+        "Which of the following class in Bootstrap is used to create a button as a link?",
+      ans: 2,
+      option: ["None of the above", ".btn-hyperlink", ".btn-link", ".btn-anchor"],
+    },
+    {
+      question: "The class xs in Bootstrap is used for ",
+      ans: 1,
+      option: ["All of the above", "  phones  ", "tablets", "desktop"],
+    },
+    {
+      question:
+        "Which of the following statement is correct about the Bootstrap wells",
+      ans: 0,
+      option:[
+        "All of the above",
+        "By default, the size of wells is medium.",
+        "Using .well-sm and .well-lg classes, we can change the size of wells.",
+        "The .well-sm class is for small wells, and the .well-lg class is for large wells. And both classes should be used in conjunction with .well class.",
+      ],
+    },
+  ];
