987fd2c4a2492e8d0d96eb6d4a39324ffa60b568
Harsh_Bhange <120706512+harshbhange0@users.noreply.github.com>
2023-05-19 15:24:41 +0530

    error resolved 5

--- src/components/AllQuizes/JavaQuiz.js
+++ src/components/AllQuizes/JavaQuiz.js
@@ -2,13 +2,17 @@ import React from 'react'
 import { useState } from "react";
 import { javaQuestion } from"../question/javaQuestion";
 import Scoreboard from "../Scoreboard";
-import javaLangSvg from "../langIconData/JavaSvg";
+import JavaSvg from "../langIconData/JavaSvg";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+import SideBarSection from "../SideBar";
 
 function JavaQuiz() {
-    const [queIndex, setQueIndex] = useState(0);
-    const [trackClick, SetTrackClick] = useState(0);
+  const [queIndex, setQueIndex] = useState(0);
 
-  function javahangeQue() {
+  const [trackClick, SetTrackClick] = useState(0);
+
+  function changeQue() {
     if (queIndex < javaQuestion.length - 1) {
       setQueIndex(queIndex + 1);
       updateScore();
@@ -24,11 +28,11 @@ function JavaQuiz() {
       return;
     }
   }
-  const [javahangeScore, setChangeScore] = useState(0);
+  const [changeScore, setChangeScore] = useState(0);
 
   function updateScore() {
     if (trackClick === javaQuestion[queIndex].ans) {
-      setChangeScore(javahangeScore + 1);
+      setChangeScore(changeScore + 1);
     } else {
       console.log(javaQuestion[queIndex].ans);
     }
@@ -36,66 +40,101 @@ function JavaQuiz() {
 
   return (
     <div>
+    <div className="QuizeSction">
       <div>
-        <div className="container">
-          <div className="score-section d-flex">
-            <Scoreboard allQuestion={javaQuestion.length} nowScore={javahangeScore} />
-            <>
-              <div className="d-flex m-2 w-100 rounded border justify-content-center align-items-center">
-                <span className="svg d-block mx-2 p-1">
-                  <javaLangSvg width="60" />
-                </span>
-                <span className="name d-block mx-2 fs-2 ">java Language</span>
-              </div>
-            </>
+        <div className="QuizeSction">
+          <div className="top-navbar">
+            <Navbar />
           </div>
-        </div>
-        <div className="container-fluid mt-5 m-auto p-2 p-sm-3 p-md-5 rounded question-sction">
-          <div className="row">
-            <div className="col-12">
-              <div className="mb-5">
-                <span className="d-block fs-2  w-100 text-center">
-                  {queIndex + 1}.{javaQuestion[queIndex].question}
-                </span>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="d-none d-sm-none d-md-flex col-md-3 border border-bottom-0 side-Sction-Holder ">
+                <SideBarSection />
               </div>
-            </div>
-            <div className="col-12">
-              <div className="w-100 d-flex flex-column justify-content-center align-items-center">
-                {javaQuestion[queIndex].option.map((option, i) => {
-                  return (
-                    <button
-                      className="btn position-relative w-75 btn-option shadow rounded-1 border-1 my-2 fs-5 p-2"
-                      onClick={function trackScore() {
-                        SetTrackClick(i + 1);
-                      }}
-                    >
-                      <span className=" position-absolute que-number">
-                        {i + 1}.{" "}
-                      </span>
-                      {option}
-                    </button>
-                  );
-                })}
-              </div>
-            </div>
-          </div>
+              <div className="col-12 col-sm-12 col-md-9">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-12 ">
+                      <div className="question-section-holder" />
+                      <div className="container">
+                        <div className="score-section d-flex">
+                          <Scoreboard
+                            allQuestion={javaQuestion.length}
+                            nowScore={changeScore}
+                          />
+                          <>
+                            <div className="d-flex m-2 w-100 rounded border justify-content-center align-items-center">
+                              <span className="svg d-block mx-2 p-1">
+                                < JavaSvg width="60" />
+                              </span>
+                              <span className="name d-block mx-2 fs-2 ">
+                              JAVA
+                              </span>
+                            </div>
+                          </>
+                        </div>
+                      </div>
+                      <div className="container-fluid mt-5 m-auto p-2 p-sm-3 p-md-5 rounded question-sction">
+                        <div className="row">
+                          <div className="col-12">
+                            <div className="mb-5">
+                              <span className="d-block fs-2  w-100 text-center">
+                                {queIndex + 1}.
+                                {javaQuestion[queIndex].question}
+                              </span>
+                            </div>
+                          </div>
+                          <div className="col-12">
+                            <div className="w-100 d-flex flex-column justify-content-center align-items-center">
+                              {javaQuestion[queIndex].option.map(
+                                (option, i) => {
+                                  return (
+                                    <button
+                                    key={i} 
+                                      className="btn position-relative w-75 btn-option shadow rounded-1 border-1 my-2 fs-5 p-2"
+                                      onClick={function trackScore() {
+                                        SetTrackClick(i + 1);
+                                      }}
+                                    >
+                                      <span className=" position-absolute que-number">
+                                        {i + 1}.{" "}
+                                      </span>
+                                      {option}
+                                    </button>
+                                  );
+                                }
+                              )}
+                            </div>
+                          </div>
+                        </div>
 
-          <div className="d-flex mt-5 justify-content-center w-100">
-            <button
-              className="btn priv-btn shadow-sm d-block mx-auto p-2 rounded-2 w-25"
-              onClick={priQue}
-            >
-              Priv
-            </button>
-            <button
-              className="btn next-btn shadow-sm d-block mx-auto p-2 rounded-2 w-25"
-              onClick={javahangeQue}
-            >
-              Save & Next
-            </button>
+                        <div className="d-flex mt-5 justify-content-center w-100">
+                          <button
+                            className="btn priv-btn shadow-sm d-block mx-auto p-2 rounded-2 w-25"
+                            onClick={priQue}
+                          >
+                            Prev
+                          </button>
+                          <button
+                            className="btn next-btn shadow-sm d-block mx-auto p-2 rounded-2 w-25"
+                            onClick={changeQue}
+                          >
+                            Save & Next
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="footer">
+                <Footer />
+              </div>
+            </div>
           </div>
         </div>
       </div>
+    </div>
     </div>
   );
   
